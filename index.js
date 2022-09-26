a1889b3596e2a4e34076daa5d44c772ba02f7b26
WDamaris <damarisshish@gmail.com>
2022-09-26 03:19:04 +0300

    login status

--- index.js
+++ index.js
@@ -24,10 +24,34 @@ db.query(
     "INSERT INTO user_credentials(userName,userEmail,userPassword) VALUES(?,?,?)",
 [userName,userEmail,userPassword],
 (err,result) =>{
-    console.log(result)
+    console.log(err)
+ }
+)
+})
+
+app.post("/login", (req,res) => {
+    const userName=req.body.userName;
+    const userEmail=req.body.userEmail;
+    const userPassword=req.body.userPassword;
+db.query(
+    "SELECT * FROM user_credentials WHERE userName=?, userEmail=?, AND userPassword=?"
+[userName,userEmail,userPassword],
+(err,result) =>{
+
+    if(err){
+        res.send({err: err})
+   }
+
+    if(result.length>0){
+        res.send(result)
+    }else{
+        res.send({message: "email and password do not match"})
+    }
+   
  }
 )
 })
 app.listen(3005,()=>{
     console.log("running in port 4000")
 })
+
